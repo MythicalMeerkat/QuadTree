696572f45a27f4977c0c354dfec890b329080816
Jeff Wilson <MythicalMeerkat@users.noreply.github.com>
2021-02-09 02:12:39 -0600

    Add files via upload

--- QuadTree.py
+++ QuadTree.py
@@ -1,24 +1,74 @@
 # Jeff Wilson
 
 import cv2
-from numpy import asarray
+import numpy
+
+
+def split(imageArr, x, y, width, height, var_threshold):
+    # Number of Pixels in the quadrant
+    # pixels = (height - x) * (width - y)
+    pixels = imageArr.size
+
+    # Split imageArr into Quadrant
+    # print(imageArr)
+
+    # Calculate the Mean value of the Pixels in the Quadrant
+    mean = (numpy.mean(imageArr, axis=(0, 1)))
+
+    # Calculate the Variance of the Quadrant
+    variance = numpy.var(imageArr)
+
+    if pixels <= 1:
+        # Image Processing/Output
+        return None
+    if variance <= var_threshold:
+        imageArr[0:width, 0:height] = mean
+        print('Filling Pixels in Quadrant! \n')
+        print('Amount of Pixels in Quadrant: ' + str(pixels) + '\n')
+        print('Variance of the Quadrant: ' + str(variance) + '\n')
+        print('Mean of Pixel Values: ' + str(mean) + '\n')
+        print(imageArr)
+    else:
+        split(imageArr[x:x+width//2, y:y+height//2], x, y, width//2, height//2, var_threshold)  # Top Left
+        split(imageArr[x+width//2:x+width, y:y+height//2], x + width//2, y, width // 2, height // 2, var_threshold)  # Top Right
+        split(imageArr[x:x+width//2, y+height//2:y+height], x, y + width//2, width // 2, height // 2, var_threshold)  # Bottom Left
+        split(imageArr[x+width//2:x+width, y+height//2:y+height], x + width//2, y + height//2, width // 2, height // 2, var_threshold)  # Bottom Right
+        print(imageArr)
+
+
+# Client Input
+variance_threshold = input('Enter the variance threshold: ')
+variance_threshold = int(variance_threshold)
 
 # Read .pgma image into Numpy Array
-im = cv2.imread('baboon.pgma')
-image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+image = cv2.imread('baboon.pgma', -1)
+image_copy = cv2.imread('baboon.pgma', -1)
+
 
 # Note to Self: Result of loading with cv2 is a Numpy Array!
-print('Type of Image after CV2: ' + str(type(image)) + '\n')
+print('--- Image Properties --- \n')
+print('Type: ' + str(type(image)) + '\n')
 
 
 # Summarize shape
-print('Shape of Image after CV2: ' + str(image.shape) + '\n')
+print('Shape: ' + str(image.shape) + '\n')
 
 # Printing Raw Data of Image
-print('Printing Data of Image: \n')
-print(image)
+# print('Image Data: \n')
+# print(image)
 
-# TODO: Look into creating recursive split function. Refer to GM.
+# Start Quad Tree Process
+split(image, 0, 0, 256, 256, variance_threshold)
+
+# Image Processing/Output
+print('Printing Image! \n')
+cv2.imwrite('result_baboon.pgm', image)
+
+
+
+
+
+
 
 
 
